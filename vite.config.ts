383ce628d979521ff9ea8b2d357eff20e43c7e42
cuongngovuihocai <cuongngovuihocai@gmail.com>
2026-08-21 01:14:27 +0700

    Update base URL based on NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/MaSoi/',
+    base: process.env.NODE_ENV === 'production' ? '/MaSoi/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
